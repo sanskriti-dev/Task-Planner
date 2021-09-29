43f206c2f9c46da611076dca6acd25378549df93
SanskritiSaluja <sanskriti.saluja@tothenew.com>
2021-09-30 02:22:07 +0530

    Create Task

--- src/components/Dasboard/index.js
+++ src/components/Dasboard/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from '../Board';
 import Header from '../Common/Header';
 import './index.scss'
 import {PlusOutlined } from '@ant-design/icons';
+import 'antd/dist/antd.css';
+import { Modal } from 'antd';
+import CreateTask from '../Tasks/dependencies/Create';
 
 const Dashboard = () => {
+    const [isModalVisible,setIsModalVisible] = useState(false)
     const boards = [{
         title : "Backlog",
         key: 'backlog',
@@ -36,10 +40,12 @@ const Dashboard = () => {
             boards.map(ele => <Board key_name={ele.key} title= {ele?.title} tasks = {ele?.list}/>)
         }
         </div> 
-        <div className = 'create-new'>
-        <PlusOutlined /> 
-        </div>   
+        <button className='create-new' onClick = {() => setIsModalVisible(true)}><PlusOutlined /></button>   
         </div> 
+
+        <Modal title="Create New Task" visible={isModalVisible} onCancel = {() => setIsModalVisible(false)} footer ={null} >
+              <CreateTask/>
+       </Modal>
         </>
         )
 }
---
+++ src/components/Tasks/dependencies/Create/index.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import { Form, Input, Button, Select} from 'antd';
+
+const { Option } = Select;
+
+const CreateTask = (props) => {
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <Form
+      name="basic"
+    //   labelCol={{
+    //     span: 8,
+    //   }}
+    //   wrapperCol={{
+    //     span: 16,
+    //   }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Title"
+        name="title"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Description"
+        name="description"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
+      >
+          <Input.TextArea />
+      </Form.Item>
+
+    <Form.Item
+        label="Priority"
+        name="priority"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Select>
+        <Option>ABC</Option>
+        </Select>
+      </Form.Item>
+      <Form.Item
+        label="Assignee"
+        name="assignee"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Select>
+        <Option>ABC</Option>
+        </Select>
+      </Form.Item>
+
+      <Form.Item
+        label="Status"
+        name="status"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Select>
+        <Option>ABC</Option>
+        </Select>
+      </Form.Item>
+
+    
+
+      <Form.Item
+        // wrapperCol={{
+        //   offset: 8,
+        //   span: 16,
+        // }}
+      >
+        <Button type="primary" htmlType="submit">
+          Create
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default CreateTask
